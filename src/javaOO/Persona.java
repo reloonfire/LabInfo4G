a3af1ca79b43e7ad7374e0385e42fba767f3b4ab
4G-IT <4G-IT@INFO1-PC13>
2018-10-09 10:57:05 +0200

    Aggiunto Package JavaOO per la programmazione ad oggetti

---
+++ src/javaOO/Persona.java
@@ -0,0 +1,10 @@
+package javaOO;
+
+public class Persona {
+	
+	String nome;
+	String cognome;
+	int eta;
+	char sesso;
+	
+}
---
+++ src/javaOO/Principale.java
@@ -0,0 +1,10 @@
+package javaOO;
+
+public class Principale {
+	public static void main(String[] args) {
+		
+		Archivio a = new Archivio();
+		a.
+		
+	}
+}
