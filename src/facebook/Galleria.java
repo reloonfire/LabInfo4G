085bc90ca08db0b022a72eb736eebfaa2a55d5a7
reloonfire <lorenzo.avallone01@gmail.com>
2018-10-20 18:13:03 +0200

    Fail with ArrayList

--- src/facebook/Galleria.java
+++ src/facebook/Galleria.java
@@ -1,23 +1,23 @@
 package facebook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Galleria {
 	
-	private Foto[] galleria = new Foto[100];
+	private List<Foto> galleria = new ArrayList<>();
+	//private Foto[] galleria = new Foto[100];
 	static int indice = 0;
 	
 	public void addFoto(Foto foto) {
-		if (indice++ < galleria.length) {
-			galleria[indice] = foto;
+			galleria.add(foto);
 			indice++;
-		}else {
-			System.out.println("Hai raggiunto il limite di foto.");
-		}
 	}
 	
 	public Foto searchPhotoByTag(String tag) {
-		for (int i = 0; i < indice; i++) {
-			if (galleria[i].getTag().equals(tag)) {
-				return galleria[i];
+		for (int i = 0; i < galleria.size(); i++) {
+			if (galleria.get(i).getTag().equals(tag)) {
+				return galleria.get(i);
 			}
 		}
 		return null;
